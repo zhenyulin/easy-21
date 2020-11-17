31e52f153a620df51807d79faf58c69b19568e24
Zhenyu Lin <zhenyu.lyn@gmail.com>
2020-11-17 01:30:17 +0000

    feat(function_approx): value_approximator and test

---
+++ src/__tests__/test_value_approximator.py
@@ -0,0 +1,123 @@
+import pytest
+
+import numpy as np
+
+from unittest import mock
+from src.value_approximator import ValueApproximator
+from src.value_map import ValueMap
+
+
+class TestInit:
+    def test_init_default_feature_function(self):
+        value_approximator = ValueApproximator("value_approximator")
+        assert value_approximator.name == "value_approximator"
+        assert value_approximator.feature_function(1) == 1
+        assert type(value_approximator.weights) is np.ndarray
+        assert type(value_approximator.cache) is np.ndarray
+        assert value_approximator.metrics_history["diff"] == []
+
+    def test_custom_feature_function(self):
+        def feature_function(input):
+            return [x * 2 for x in input]
+
+        value_approximator = ValueApproximator(
+            "value_approximator", feature_function=feature_function
+        )
+        assert value_approximator.feature_function([1]) == [2]
+
+
+class TestFeatures:
+    def test_return_np_array(self):  # for feature_function return normal list
+        value_approximator = ValueApproximator("value_approximator")
+        value_approximator.feature_function = lambda x: [i * 2 for i in x]
+        features = value_approximator.features([1, 2])
+        assert type(features) is np.ndarray
+        assert np.array_equal(features, [2, 4])
+
+        # for feature function return np.ndarray
+        value_approximator.feature_function = lambda x: np.array(x) * 2
+        features = value_approximator.features([1])
+        assert type(features) is np.ndarray
+        assert np.array_equal(features, [2])
+
+
+class TestGet:
+    def test_init_weights_by_feature_size(self):
+        value_approximator = ValueApproximator("value_approximator")
+        input = [1, 2, 3]
+        value_approximator.get(input)
+        assert value_approximator.weights.size == 3
+
+    def test_output_value(self):
+        value_approximator = ValueApproximator("value_approximator")
+        value_approximator.weights = np.array([1.0, 1.0, 1.0])
+        input = [1, 2, 3]
+        value = value_approximator.get(input)
+        assert type(value) is np.float64
+        assert value == 6.0
+
+    def test_output_features(self):
+        value_approximator = ValueApproximator("value_approximator")
+        value_approximator.feature_function = lambda x: [i * 2 for i in x]
+        input = [1, 2, 3]
+        value, features = value_approximator.get(input, output_features=True)
+        assert type(features) is np.ndarray
+        assert np.array_equal(features, [2, 4, 6])
+
+
+class Testlearn:
+    def test_update_weights(self):
+        value_approximator = ValueApproximator("value_approximator")
+        value_approximator.weights = np.array([1.0, 1.0, 1.0])
+        assert np.array_equal(value_approximator.weights, [1.0, 1.0, 1.0])
+        sample_input = [1, 2, 3]
+        sample_target = 10
+        value_approximator.learn(sample_input, sample_target, step_size=0.1)
+        assert np.allclose(value_approximator.weights, [0.6, 0.2, -0.2])
+
+
+class TestBackup:
+    def test_backup_to_cache(self):
+        value_approximator = ValueApproximator("value_approximator")
+        value_approximator.weights = np.array([1.0, 1.0, 1.0])
+        value_approximator.backup()
+        assert np.allclose(value_approximator.weights, value_approximator.cache)
+
+
+class TestReset:
+    def test_reset(self):
+        value_approximator = ValueApproximator("value_approximator")
+        value_approximator.weights = np.array([1.0, 1.0, 1.0])
+        value_approximator.backup()
+        value_approximator.reset()
+        assert np.array_equal(value_approximator.weights, [])
+        assert np.array_equal(value_approximator.cache, [])
+
+
+class TestDiff:
+    def test_diff_return_correct(self):
+        value_approximator = ValueApproximator("value_approximator")
+        value_approximator.weights = np.array([1.0, 1.0, 1.0])
+        value_approximator.backup()
+        value_approximator.weights = np.array([1.2, 0.8, 1.2])
+        assert abs(value_approximator.diff() - 0.04) < 1e-5
+
+
+def test_compare_value_map():
+    value_approximator = ValueApproximator("value_approximator")
+    value_approximator.weights = np.array([1.0, 1.0, 1.0])
+    value_approximator.feature_function = lambda x: [*x, 1]
+    value_map = ValueMap("value_map")
+    value_map.set((1, 2), 1)
+    value_map.set((2, 2), 3)
+    assert value_approximator.compare_value_map(value_map) - np.sqrt(6.5) < 1e-5
+
+
+def test_record():
+    value_approximator = ValueApproximator("value_approximator")
+    value_approximator.weights = np.array([1.0, 1.0, 1.0])
+    value_approximator.backup()
+    value_approximator.weights = np.array([1.2, 0.8, 1.2])
+    value_approximator.record(["diff"])
+    assert np.allclose(value_approximator.metrics_history["diff"], [0.04])
+    assert np.allclose(value_approximator.weights, value_approximator.cache)
---
+++ src/value_approximator.py
@@ -0,0 +1,122 @@
+import numpy as np
+
+
+class ValueApproximator:
+    """ValueApproximator
+
+    A model to store trainable parameters
+    to output values from features
+
+    Unlike using a table key lookup in ValueMap,
+    it uses the compressed feature vectors x weights
+    to approximate the variaties of state possibilities
+    as a position in the state space
+
+    feature function: input values -> features
+    """
+
+    def __init__(self, name, feature_function=lambda x: x):
+        self.name = name
+        self.feature_function = feature_function
+
+        self.weights = np.array([])
+        self.cache = np.array([])
+        self.metrics_history = {
+            "diff": [],
+        }
+
+    #
+    # utility functions
+    #
+    def features(self, input):
+        features = self.feature_function(input)
+        return features if type(features) is np.ndarray else np.array(features)
+
+    def init_weights_if_not_yet(self, features):
+        if self.weights.size == 0:
+            self.weights = np.random.random_sample(features.shape)
+
+    #
+    # getter functions
+    #
+    def get(self, input, output_features=False):
+        features = self.features(input)
+        self.init_weights_if_not_yet(features)
+        value = np.dot(np.transpose(features), self.weights)
+        return (value, features) if output_features else value
+
+    #
+    # setter functions
+    #
+    def learn(self, sample_input, sample_target, step_size=0.01):
+        value, features = self.get(sample_input, output_features=True)
+        # using mean squared error as the objective function
+        # O(weights) = E[(target - features * weights)**2]
+        derivative = features
+        error = sample_target - value
+        gradient = step_size * error * derivative
+        self.weights -= gradient
+
+    def backup(self):
+        self.cache = np.copy(self.weights)
+
+    def reset(self):
+        self.weights = np.array([])
+        self.cache = np.array([])
+
+    #
+    # metrics functions
+    #
+    def diff(self):
+        if self.cache.size == 0:
+            self.cache = np.zeros_like(self.weights)
+        return (np.square(self.weights - self.cache)).mean(axis=0)
+
+    def compare_value_map(self, value_map):
+        sq_error = 0
+        for key in value_map.keys():
+            other_value = value_map.get(key)
+            error = self.get(key) - other_value
+            sq_error += error ** 2
+
+        return np.sqrt(sq_error / len(value_map.keys()))
+
+    #
+    # metrics history function
+    #
+    def record(self, metrics_names, log=True):
+        for metrics_name in metrics_names:
+            metrics_method = getattr(self, metrics_name)
+            metrics = metrics_method()
+            self.metrics_history[metrics_name].append(metrics)
+            if log:
+                print(f"{self.name}_{metrics_name}: {metrics:.3f}")
+
+            if metrics_name == "diff":
+                self.backup()
+
+    def converged(self, metrics_name, threshold, log=True):
+        last_3 = self.metrics_history[metrics_name][-4:-1]
+
+        if len(last_3) < 3:
+            return False
+
+        last_3_mean = sum(last_3) / len(last_3)
+
+        if last_3_mean < threshold:
+            if log:
+                print(f"{self.name}_{metrics_name} has converged")
+            return True
+        else:
+            return False
+
+    #
+    # file I/O functions
+    #
+    def save(self, path):
+        with open(path, "w") as f:
+            np.save(f, self.weights)
+
+    def load(self, path):
+        with open(path, "r") as f:
+            self.weights = np.load(f)
