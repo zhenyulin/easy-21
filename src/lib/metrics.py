10bf6da7bfe3904520196e379a46d572c8dbf086
Zhenyu Lin <zhenyu.lyn@gmail.com>
2020-12-03 18:09:01 +0000

    feat(metrics): plot_history_stack now show variance between runs

--- src/lib/metrics.py
+++ src/lib/metrics.py
@@ -45,16 +45,6 @@ class Metrics:
         else:
             return False
 
-    def stack_variance(self, name, indexes):
-        # self.pad_stack_length(name)
-
-        stack = self.history_stack[name]
-        [i, j] = indexes
-        zipped_stack = zip(stack[i], stack[j])
-
-        sq_error = [(a - b) ** 2 for (a, b) in zipped_stack]
-        return sq_error / len(zipped_stack)
-
     #
     # setter functions
     #
@@ -116,13 +106,15 @@ class Metrics:
 
         ax.plot(x, y)
 
+    # TODO: incorporate spaghetti plot
+    # https://python-graph-gallery.com/125-small-multiples-for-line-chart/
     def plot_history_stack(
         self,
         name,
         x=None,
+        labels=None,
         title=None,
         figsize=(18, 18),
-        labels=None,
     ):
         plt.figure(figsize=figsize)
 
@@ -132,21 +124,36 @@ class Metrics:
         self.pad_stack_length(name)
         history_stack = self.history_stack[name]
 
-        if x is None:
-            x = np.arange(1, len(history_stack[0]) + 1, 1)
+        dfs = [
+            pd.DataFrame(
+                {
+                    "x": range(1, len(d) + 1) if x is None else x,
+                    "value": d,
+                    "label": None if labels is None else labels[i % len(labels)],
+                    "run": i if labels is None else i // len(labels),
+                }
+            )
+            for i, d in enumerate(history_stack)
+        ]
 
-        if len(x) < 30:
-            plt.xticks(x)
+        df = pd.concat(dfs)
 
-        label_map = {}
-        for i, y in enumerate(history_stack):
-            label = str(i) if labels is None else labels[i % len(labels)]
-
-            (ax,) = plt.plot(x, y)
-
-            if label not in label_map:
-                label_map[label] = []
-
-            label_map[label].append(ax)
-
-        plt.legend(map(lambda x: tuple(x), label_map.values()), label_map.keys())
+        if labels is not None and len(labels) > 3:
+            sns.lineplot(
+                data=df,
+                x="x",
+                y="value",
+                hue="label",
+                style="run",
+            )
+            sns.relplot(
+                data=df,
+                x="x",
+                y="value",
+                hue="label",
+                col="label",
+                col_wrap=2,
+                kind="line",
+            )
+        else:
+            sns.lineplot(data=df, x="x", y="value", hue="label")
